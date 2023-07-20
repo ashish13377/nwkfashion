f6dfe5a6f091a39c262611ab72cb1b018c6e8eac
amaan laskar <laskaramaan@gmail.com>
2023-07-20 11:15:52 +0530

    footer fixed

--- client/src/layouts/footer.js
+++ client/src/layouts/footer.js
@@ -11,17 +11,12 @@ export default function footer() {
             <div className="row mbn-40">
               <div className="footer-widget col-lg-3 col-md-6 col-12 mb-40">
                 <h4 className="title">CONTACT US</h4>
-                <p>
-                  You address will be here
-                  <br /> Lorem Ipsum text
-                </p>
+
                 <p>
                   <a href="tel:01234567890">01234 567 890</a>
-                  <a href="tel:01234567891">01234 567 891</a>
                 </p>
                 <p>
                   <a href="mailto:info@example.com">info@example.com</a>
-                  <a href="#">www.example.com</a>
                 </p>
               </div>
               <div className="footer-widget col-lg-3 col-md-6 col-12 mb-40">
@@ -30,20 +25,12 @@ export default function footer() {
                   <li>
                     <a href="#">New Arrivals</a>
                   </li>
-                  <li>
-                    <a href="#">Best Seller</a>
-                  </li>
-                  <li>
-                    <a href="#">Trendy Items</a>
-                  </li>
+
                   <li>
                     <a href="#">Best Deals</a>
                   </li>
                   <li>
                     <a href="#">On Sale Products</a>
-                  </li>
-                  <li>
-                    <a href="#">Featured Products</a>
                   </li>
                 </ul>
               </div>
@@ -56,47 +43,13 @@ export default function footer() {
                   <li>
                     <a href="#">Terms &amp; Conditions</a>
                   </li>
-                  <li>
-                    <a href="#">Payment Method</a>
-                  </li>
-                  <li>
-                    <a href="#">Product Warranty</a>
-                  </li>
+
                   <li>
                     <a href="#">Return Process</a>
-                  </li>
-                  <li>
-                    <a href="#">Payment Security</a>
                   </li>
                 </ul>
               </div>
               <div className="footer-widget col-lg-3 col-md-6 col-12 mb-40">
-                <h4 className="title">NEWSLETTER</h4>
-                <p>
-                  Subscribe our newsletter and get all update of our product
-                </p>
-                <form id="mc-form" className="mc-form footer-subscribe-form">
-                  <input
-                    id="mc-email"
-                    autoComplete="off"
-                    placeholder="Enter your email here"
-                    name="EMAIL"
-                    type="email"
-                  />
-                  <button id="mc-submit">
-                    <i className="fa fa-paper-plane-o" />
-                  </button>
-                </form>
-                {/* mailchimp-alerts Start */}
-                <div className="mailchimp-alerts">
-                  <div className="mailchimp-submitting" />
-                  {/* mailchimp-submitting end */}
-                  <div className="mailchimp-success" />
-                  {/* mailchimp-success end */}
-                  <div className="mailchimp-error" />
-                  {/* mailchimp-error end */}
-                </div>
-                {/* mailchimp-alerts end */}
                 <h5>FOLLOW US</h5>
                 <p className="footer-social">
                   <a href="#">Facebook</a> - <a href="#">Twitter</a> -{" "}
