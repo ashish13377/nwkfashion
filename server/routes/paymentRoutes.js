e6e18a0e1cab46b4166db6ca098e323aad2d8635
amaan laskar <laskaramaan@gmail.com>
2023-08-14 13:08:29 +0530

    account details done

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,11 +1,13 @@
 // routes/paymentRoutes.js
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createOrder, confirmPayment } = require('../controllers/paymentController');
+const {
+  createOrder,
+  confirmPayment,
+} = require("../controllers/paymentController");
 
 // Define routes for payment handling
-router.post('/createOrder', createOrder);
-router.post('/confirmPayment', confirmPayment);
+router.post("/createOrder", createOrder);
+router.post("/confirmPayment", confirmPayment);
 
 module.exports = router;
