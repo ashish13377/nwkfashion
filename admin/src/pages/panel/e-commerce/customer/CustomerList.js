f459dda9503310369254117a160a3dbe00d55eab
ashish13377 <trojan2277@gmail.com>
2023-08-25 10:40:47 +0530

    new changes

--- admin/src/pages/panel/e-commerce/customer/CustomerList.js
+++ admin/src/pages/panel/e-commerce/customer/CustomerList.js
@@ -32,10 +32,16 @@ import { Link } from "react-router-dom";
 import { CustomerContext } from "./CustomerContext";
 import EditModal from "./EditModal";
 import AddModal from "./AddModal";
+import axios from "axios";
+import { serverAPI } from "../../../..";
+import { Spinner } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Swal from "sweetalert2";
 
 const CustomerList = () => {
   const { contextData } = useContext(CustomerContext);
-  const [data, setData] = contextData;
+  const [data, setData] = useState([]);
 
   const [sm, updateSm] = useState(false);
   const [onSearchText] = useState("");
@@ -61,135 +67,153 @@ const CustomerList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemPerPage] = useState(10);
 
-  // unselects the data on mount
-  useEffect(() => {
-    let newData;
-    newData = CustomerData.map((item) => {
-      item.checked = false;
-      return item;
-    });
-    setData([...newData]);
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  // Changing state value when searching name
-  useEffect(() => {
-    if (onSearchText !== "") {
-      const filteredObject = CustomerData.filter((item) => {
-        return (
-          item.name.toLowerCase().includes(onSearchText.toLowerCase()) ||
-          item.email.toLowerCase().includes(onSearchText.toLowerCase())
-        );
+  const getOrdersdata = async () => {
+    // Make the API call to fetch the product
+    await axios
+      .get(`${serverAPI}users/users`)
+      .then((response) => {
+        // Assuming the response data is in the format you provided
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching product:", error);
       });
-      setData([...filteredObject]);
-    } else {
-      setData([...CustomerData]);
-    }
-  }, [onSearchText, setData]);
+  };
+  useEffect(() => {
+    getOrdersdata();
+  }, [data]); // Fetch subcategories when selected category changes
+
+
+  // // unselects the data on mount
+  // useEffect(() => {
+  //   let newData;
+  //   newData = CustomerData.map((item) => {
+  //     item.checked = false;
+  //     return item;
+  //   });
+  //   setData([...newData]);
+  // }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // // Changing state value when searching name
+  // useEffect(() => {
+  //   if (onSearchText !== "") {
+  //     const filteredObject = CustomerData.filter((item) => {
+  //       return (
+  //         item.name.toLowerCase().includes(onSearchText.toLowerCase()) ||
+  //         item.email.toLowerCase().includes(onSearchText.toLowerCase())
+  //       );
+  //     });
+  //     setData([...filteredObject]);
+  //   } else {
+  //     setData([...CustomerData]);
+  //   }
+  // }, [onSearchText, setData]);
 
   // function to change the selected property of an item
   const onSelectChange = (e, id) => {
     let newData = data;
-    let index = newData.findIndex((item) => item.id === id);
+    let index = newData.findIndex((item) => item._id === id);
     newData[index].checked = e.currentTarget.checked;
     setData([...newData]);
   };
 
-  // function to reset the form
-  const resetForm = () => {
-    setFormData({
-      name: "",
-      email: "",
-      balance: 0,
-      phone: "",
-      status: "Active",
-    });
-  };
+  // // function to reset the form
+  // const resetForm = () => {
+  //   setFormData({
+  //     name: "",
+  //     email: "",
+  //     balance: 0,
+  //     phone: "",
+  //     status: "Active",
+  //   });
+  // };
 
-  const closeModal = () => {
-    setModal({ add: false })
-    resetForm();
-  };
+  // const closeModal = () => {
+  //   setModal({ add: false })
+  //   resetForm();
+  // };
 
-  const closeEditModal = () => {
-    setModal({ edit: false })
-    resetForm();
-  };
+  // const closeEditModal = () => {
+  //   setModal({ edit: false })
+  //   resetForm();
+  // };
 
-  // submit function to add a new item
-  const onFormSubmit = (submitData) => {
-    const { name, email, balance, phone } = submitData;
-    let submittedData = {
-      id: data.length + 1,
-      avatarBg: "purple",
-      name: name,
-      role: "Customer",
-      email: email,
-      balance: balance,
-      phone: phone,
-      emailStatus: "success",
-      kycStatus: "alert",
-      lastLogin: "10 Feb 2020",
-      status: formData.status,
-      country: "Bangladesh",
-    };
-    setData([submittedData, ...data]);
-    resetForm();
-    setModal({ edit: false }, { add: false });
-  };
+  // // submit function to add a new item
+  // const onFormSubmit = (submitData) => {
+  //   const { name, email, balance, phone } = submitData;
+  //   let submittedData = {
+  //     id: data.length + 1,
+  //     avatarBg: "purple",
+  //     name: name,
+  //     role: "Customer",
+  //     email: email,
+  //     balance: balance,
+  //     phone: phone,
+  //     emailStatus: "success",
+  //     kycStatus: "alert",
+  //     lastLogin: "10 Feb 2020",
+  //     status: formData.status,
+  //     country: "Bangladesh",
+  //   };
+  //   setData([submittedData, ...data]);
+  //   resetForm();
+  //   setModal({ edit: false }, { add: false });
+  // };
 
-  // submit function to update a new item
-  const onEditSubmit = (submitData) => {
-    const { name, email, phone } = submitData;
-    let submittedData;
-    let newitems = data;
-    newitems.forEach((item) => {
-      if (item.id === editId) {
-        submittedData = {
-          id: item.id,
-          avatarBg: item.avatarBg,
-          name: name,
-          image: item.image,
-          role: item.role,
-          email: email,
-          balance: editFormData.balance,
-          phone: phone,
-          emailStatus: item.emailStatus,
-          kycStatus: item.kycStatus,
-          lastLogin: item.lastLogin,
-          status: editFormData.status,
-          country: item.country,
-        };
-      }
-    });
-    let index = newitems.findIndex((item) => item.id === editId);
-    newitems[index] = submittedData;
-    setModal({ edit: false });
-  };
+  // // submit function to update a new item
+  // const onEditSubmit = (submitData) => {
+  //   const { name, email, phone } = submitData;
+  //   let submittedData;
+  //   let newitems = data;
+  //   newitems.forEach((item) => {
+  //     if (item.id === editId) {
+  //       submittedData = {
+  //         id: item.id,
+  //         avatarBg: item.avatarBg,
+  //         name: name,
+  //         image: item.image,
+  //         role: item.role,
+  //         email: email,
+  //         balance: editFormData.balance,
+  //         phone: phone,
+  //         emailStatus: item.emailStatus,
+  //         kycStatus: item.kycStatus,
+  //         lastLogin: item.lastLogin,
+  //         status: editFormData.status,
+  //         country: item.country,
+  //       };
+  //     }
+  //   });
+  //   let index = newitems.findIndex((item) => item.id === editId);
+  //   newitems[index] = submittedData;
+  //   setModal({ edit: false });
+  // };
 
-  // function that loads the want to editted data
-  const onEditClick = (id) => {
-    data.forEach((item) => {
-      if (item.id === id) {
-        setEditFormData({
-          name: item.name,
-          email: item.email,
-          status: item.status,
-          phone: item.phone,
-          balance: item.balance,
-        });
-        setModal({ edit: true }, { add: false });
-        setEditedId(id);
-      }
-    });
-  };
+  // // function that loads the want to editted data
+  // const onEditClick = (id) => {
+  //   data.forEach((item) => {
+  //     if (item.id === id) {
+  //       setEditFormData({
+  //         name: item.name,
+  //         email: item.email,
+  //         status: item.status,
+  //         phone: item.phone,
+  //         balance: item.balance,
+  //       });
+  //       setModal({ edit: true }, { add: false });
+  //       setEditedId(id);
+  //     }
+  //   });
+  // };
 
-  // function to change to suspend property for an item
-  const suspendUser = (id) => {
-    let newData = data;
-    let index = newData.findIndex((item) => item.id === id);
-    newData[index].status = "Suspend";
-    setData([...newData]);
-  };
+  // // function to change to suspend property for an item
+  // const suspendUser = (id) => {
+  //   let newData = data;
+  //   let index = newData.findIndex((item) => item.id === id);
+  //   newData[index].status = "Suspend";
+  //   setData([...newData]);
+  // };
 
   // function to change the check property of an item
   const selectorCheck = (e) => {
@@ -201,28 +225,27 @@ const CustomerList = () => {
     setData([...newData]);
   };
 
-  // function to delete the seletected item
-  const selectorDeleteUser = () => {
-    let newData;
-    newData = data.filter((item) => item.checked !== true);
-    setData([...newData]);
-  };
+  // // function to delete the seletected item
+  // const selectorDeleteUser = () => {
+  //   let newData;
+  //   newData = data.filter((item) => item.checked !== true);
+  //   setData([...newData]);
+  // };
 
-  // function to change the complete property of an item
-  const selectorSuspendUser = () => {
-    let newData;
-    newData = data.map((item) => {
-      if (item.checked === true) item.status = "Suspend";
-      return item;
-    });
-    setData([...newData]);
-  };
+  // // function to change the complete property of an item
+  // const selectorSuspendUser = () => {
+  //   let newData;
+  //   newData = data.map((item) => {
+  //     if (item.checked === true) item.status = "Suspend";
+  //     return item;
+  //   });
+  //   setData([...newData]);
+  // };
 
   // Get current list, pagination
   const indexOfLastItem = currentPage * itemPerPage;
   const indexOfFirstItem = indexOfLastItem - itemPerPage;
   const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
-
   // Change Page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -286,39 +309,43 @@ const CustomerList = () => {
                 <span className="sub-text">User</span>
               </DataTableRow>
               <DataTableRow size="mb">
-                <span className="sub-text">Ordered</span>
-              </DataTableRow>
-              <DataTableRow size="md">
-                <span className="sub-text">Phone</span>
-              </DataTableRow>
-              <DataTableRow size="lg">
-                <span className="sub-text">Country</span>
+                <span className="sub-text">Last orders amount</span>
               </DataTableRow>
               <DataTableRow size="lg">
                 <span className="sub-text">Last Order</span>
               </DataTableRow>
+              <DataTableRow size="md">
+                <span className="sub-text">Phone</span>
+              </DataTableRow>
+              <DataTableRow size="md">
+                <span className="sub-text">Email</span>
+              </DataTableRow>
+              <DataTableRow size="lg">
+                <span className="sub-text">Country</span>
+              </DataTableRow>
+              
              
               
             </DataTableHead>
             {/*Head*/}
             {currentItems.length > 0
               ? currentItems.map((item) => (
-                  <DataTableItem key={item.id}>
+                  <DataTableItem key={item._id}>
                     <DataTableRow className="nk-tb-col-check">
                       <div className="custom-control custom-control-sm custom-checkbox notext">
                         <input
                           type="checkbox"
                           className="custom-control-input"
                           defaultChecked={item.checked}
-                          id={item.id + "uid1"}
+                          id={item._id + "uid1"}
                           key={Math.random()}
-                          onChange={(e) => onSelectChange(e, item.id)}
+                          onChange={(e) => onSelectChange(e, item._id)}
                         />
                         <label className="custom-control-label" htmlFor={item.id + "uid1"}></label>
                       </div>
                     </DataTableRow>
                     <DataTableRow>
-                      <Link to={`${process.env.PUBLIC_URL}/ecommerce/customer-details/${item.id}`}>
+                      <Link to={`${process.env.PUBLIC_URL}/ecommerce/customer-details/${item._id}`}>
                         <div className="user-card">
                           <UserAvatar theme={item.avatarBg} text={findUpper(item.name)} image={item.image}></UserAvatar>
                           <div className="user-info">
@@ -332,18 +359,22 @@ const CustomerList = () => {
                     </DataTableRow>
                     <DataTableRow size="mb">
                       <span className="tb-amount">
-                        {item.balance} <span className="currency">USD</span>
+                        {item.lastOrderAmount? <> <span className="currency">₹ </span>  {item.lastOrderAmount}</> : <>not have</>}
                       </span>
                     </DataTableRow>
+                    <DataTableRow size="lg">
+                      <span>{item.lastOrderDate ? <> {item.lastOrderDate}</> : <>not have</>}</span>
+                    </DataTableRow>
                     <DataTableRow size="md">
-                      <span>{item.phone}</span>
+                      <span>{item.phone ? <> {item.phone}</> : <>not have</>}</span>
+                    </DataTableRow>
+                    <DataTableRow size="md">
+                      <span>{item.email}</span>
                     </DataTableRow>
                     <DataTableRow size="lg">
-                      <span>{item.country}</span>
+                      <span>{item.country ? <> {item.country}</> : <>not have</>}</span>
                     </DataTableRow>
-                    <DataTableRow size="lg">
-                      <span>{item.lastLogin}</span>
-                    </DataTableRow>
+                    
                   
                   </DataTableItem>
                 ))
@@ -366,8 +397,8 @@ const CustomerList = () => {
         </Block>
 
         
-        <AddModal modal={modal.add} formData={formData} setFormData={setFormData} closeModal={closeModal} onSubmit={onFormSubmit} filterStatus={filterStatus} />
-        <EditModal modal={modal.edit} formData={editFormData} setFormData={setEditFormData} closeModal={closeEditModal} onSubmit={onEditSubmit} filterStatus={filterStatus} />
+        {/* <AddModal modal={modal.add} formData={formData} setFormData={setFormData} closeModal={closeModal} onSubmit={onFormSubmit} filterStatus={filterStatus} />
+        <EditModal modal={modal.edit} formData={editFormData} setFormData={setEditFormData} closeModal={closeEditModal} onSubmit={onEditSubmit} filterStatus={filterStatus} /> */}
         
       </Content>
     </React.Fragment>
