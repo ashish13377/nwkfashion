0295016e84df2afde22f135c55d5262ceb43ab33
amaan laskar <laskaramaan@gmail.com>
2023-08-27 19:01:11 +0530

    products api fixed

--- server/config/models/product.js
+++ server/config/models/product.js
@@ -1,97 +1,77 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define Product schema
 const productSchema = new mongoose.Schema({
   imageSrc: {
     type: String,
-    required: true,
   },
   title: {
     type: String,
-    required: true,
   },
   rating: {
     type: Number,
-    required: true,
   },
   sizes: {
     type: [String],
-    required: true,
   },
   colors: [
     {
       color: {
         type: String,
-        required: true,
       },
       zoomImage: {
         type: String,
-        required: true,
       },
     },
   ],
   price: {
     type: String,
-    required: true,
   },
   categoryId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
+    ref: "Category",
   },
   categoryName: {
     type: String,
-    required: true,
   },
   description: {
     type: String,
-    required: true,
   },
   availability: {
     type: String,
-    required: true,
   },
   quantity: {
     type: Number,
-    required: true,
   },
   socialMedia: [
     {
       name: {
         type: String,
-        required: true,
       },
       icon: {
         type: String,
-        required: true,
       },
     },
   ],
   compositions: {
     type: String,
-    required: true,
   },
   styles: {
     type: String,
-    required: true,
   },
   properties: {
     type: String,
-    required: true,
   },
   zoomImage: {
     type: String,
-    required: true,
   },
   image: {
     type: String,
-    required: true,
   },
   productType: {
     type: String,
-    required: true,
   },
 });
 
 // Create and export Product model
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,292 +1,300 @@
-const Product = require('../config/models/product');
-const Category = require('../config/models/category');
+const Product = require("../config/models/product");
+const Category = require("../config/models/category");
 
 // Get all products
 const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch (error) {
-        console.error('Error fetching products:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Get a specific product by ID
 const getProductById = async (req, res) => {
-    try {
-        const productId = req.params.productId;
-        const product = await Product.findById(productId);
-        if (!product) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-        res.json(product);
-    } catch (error) {
-        console.error('Error fetching product:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
     }
+    res.json(product);
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Create a new product
 const createProduct = async (req, res) => {
-    try {
-        const {
-            imageSrc,
-            title,
-            rating,
-            sizes,
-            colors,
-            price,
-            categoryId,
-            categoryName,
-            description,
-            availability,
-            quantity,
-            socialMedia,
-            compositions,
-            styles,
-            properties,
-            zoomImage,
-            image,
-            productType,
-        } = req.body;
+  //   console.log(req.body);
 
-        if (
-            !imageSrc ||
-            !title ||
-            !rating ||
-            !sizes ||
-            !colors ||
-            !price ||
-            !categoryId ||
-            !categoryName ||
-            !description ||
-            !availability ||
-            !quantity ||
-            !socialMedia ||
-            !compositions ||
-            !styles ||
-            !properties ||
-            !zoomImage ||
-            !image ||
-            !productType
-        ) {
-            return res.status(422).json({ error: 'Please enter all the required fields' });
-        }
+  try {
+    const {
+      imageSrc,
+      title,
+      rating,
+      sizes,
+      colors,
+      price,
+      categoryId,
+      categoryName,
+      description,
+      availability,
+      quantity,
+      socialMedia,
+      compositions,
+      styles,
+      properties,
+      zoomImage,
+      image,
+      productType,
+    } = req.body;
 
-        const category = await Category.findById(categoryId);
-        if (!category) {
-            return res.status(404).json({ error: 'Category not found' });
-        }
+    if (
+      !imageSrc ||
+      !title ||
+      !rating ||
+      !sizes ||
+      !colors ||
+      !price ||
+      !categoryId ||
+      !categoryName ||
+      !description ||
+      !availability ||
+      !quantity ||
+      !socialMedia ||
+      !compositions ||
+      !styles ||
+      !properties ||
+      !zoomImage ||
+      !productType
+    ) {
+      console.log("hello");
+      return res
 
-        const product = new Product({
-            imageSrc,
-            title,
-            rating,
-            sizes,
-            colors,
-            price,
-            categoryId: category._id,
-            categoryName,
-            description,
-            availability,
-            quantity,
-            socialMedia,
-            compositions,
-            styles,
-            properties,
-            zoomImage,
-            image,
-            productType,
-        });
-
-        const createdProduct = await product.save();
-        res.status(201).json(createdProduct);
-    } catch (error) {
-        console.error('Error creating product:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        .status(422)
+        .json({ error: "Please enter all the required fields" });
     }
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    const product = new Product({
+      imageSrc,
+      title,
+      rating,
+      sizes,
+      colors,
+      price,
+      categoryId: category._id,
+      categoryName,
+      description,
+      availability,
+      quantity,
+      socialMedia,
+      compositions,
+      styles,
+      properties,
+      zoomImage,
+      image,
+      productType,
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error("Error creating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Update a product by ID
 const updateProduct = async (req, res) => {
-    try {
-        const productId = req.params.productId;
-        const {
-            imageSrc,
-            title,
-            rating,
-            sizes,
-            colors,
-            price,
-            categoryId,
-            categoryName,
-            description,
-            availability,
-            quantity,
-            socialMedia,
-            compositions,
-            styles,
-            properties,
-            zoomImage,
-            image,
-            productType,
-        } = req.body;
+  try {
+    const productId = req.params.productId;
+    const {
+      imageSrc,
+      title,
+      rating,
+      sizes,
+      colors,
+      price,
+      categoryId,
+      categoryName,
+      description,
+      availability,
+      quantity,
+      socialMedia,
+      compositions,
+      styles,
+      properties,
+      zoomImage,
+      image,
+      productType,
+    } = req.body;
 
-        if (
-            !imageSrc ||
-            !title ||
-            !rating ||
-            !sizes ||
-            !colors ||
-            !price ||
-            !categoryId ||
-            !categoryName ||
-            !description ||
-            !availability ||
-            !quantity ||
-            !socialMedia ||
-            !compositions ||
-            !styles ||
-            !properties ||
-            !zoomImage ||
-            !image ||
-            !productType
-        ) {
-            return res.status(422).json({ error: 'Please enter all the required fields' });
-        }
-
-        const category = await Category.findById(categoryId);
-        if (!category) {
-            return res.status(404).json({ error: 'Category not found' });
-        }
-
-        const updatedProduct = await Product.findByIdAndUpdate(
-            productId,
-            {
-                imageSrc,
-                title,
-                rating,
-                sizes,
-                colors,
-                price,
-                categoryId: category._id,
-                categoryName,
-                description,
-                availability,
-                quantity,
-                socialMedia,
-                compositions,
-                styles,
-                properties,
-                zoomImage,
-                image,
-                productType,
-            },
-            { new: true }
-        );
-
-        if (!updatedProduct) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-
-        res.json(updatedProduct);
-    } catch (error) {
-        console.error('Error updating product:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (
+      !imageSrc ||
+      !title ||
+      !rating ||
+      !sizes ||
+      !colors ||
+      !price ||
+      !categoryId ||
+      !categoryName ||
+      !description ||
+      !availability ||
+      !quantity ||
+      !socialMedia ||
+      !compositions ||
+      !styles ||
+      !properties ||
+      !zoomImage ||
+      !image ||
+      !productType
+    ) {
+      return res
+        .status(422)
+        .json({ error: "Please enter all the required fields" });
     }
+
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      {
+        imageSrc,
+        title,
+        rating,
+        sizes,
+        colors,
+        price,
+        categoryId: category._id,
+        categoryName,
+        description,
+        availability,
+        quantity,
+        socialMedia,
+        compositions,
+        styles,
+        properties,
+        zoomImage,
+        image,
+        productType,
+      },
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json(updatedProduct);
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Delete a product by ID
 const deleteProduct = async (req, res) => {
-    try {
-        const productId = req.params.productId;
-        const deletedProduct = await Product.findByIdAndRemove(productId);
+  try {
+    const productId = req.params.productId;
+    const deletedProduct = await Product.findByIdAndRemove(productId);
 
-        if (!deletedProduct) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-
-        res.json({ message: 'Product deleted successfully' });
-    } catch (error) {
-        console.error('Error deleting product:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
     }
+
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Get all categories
 const getAllCate = async (req, res) => {
-    try {
-        const categories = await Category.find();
-        res.json(categories);
-    } catch (error) {
-        console.error('Error fetching categories:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Get a specific category by ID
 const getCategoryById = async (req, res) => {
-    try {
-        const categoryId = req.params.categoryId;
-        const category = await Category.findById(categoryId);
-        if (!category) {
-            return res.status(404).json({ error: 'Category not found' });
-        }
-        res.json(category);
-    } catch (error) {
-        console.error('Error fetching category:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const categoryId = req.params.categoryId;
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
     }
+    res.json(category);
+  } catch (error) {
+    console.error("Error fetching category:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Create a new category
 const createCategory = async (req, res) => {
-    try {
-        const { name } = req.body;
+  try {
+    const { name } = req.body;
 
-        if (!name) {
-            return res.status(422).json({ error: 'Please enter the category name' });
-        }
-
-        const category = new Category({
-            name,
-        });
-
-        const createdCategory = await category.save();
-        res.status(201).json(createdCategory);
-    } catch (error) {
-        console.error('Error creating category:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!name) {
+      return res.status(422).json({ error: "Please enter the category name" });
     }
+
+    const category = new Category({
+      name,
+    });
+
+    const createdCategory = await category.save();
+    res.status(201).json(createdCategory);
+  } catch (error) {
+    console.error("Error creating category:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Get all products by category name
 const getProductsByCategory = async (req, res) => {
-    try {
-        const categoryName = req.params.categoryName;
-        const category = await Category.findOne({ name: categoryName });
+  try {
+    const categoryName = req.params.categoryName;
+    const category = await Category.findOne({ name: categoryName });
 
-        if (!category) {
-            return res.status(404).json({ error: 'Category not found' });
-        }
-
-        const products = await Product.find({ categoryId: category._id });
-        res.json(products);
-    } catch (error) {
-        console.error('Error fetching products by category:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
     }
+
+    const products = await Product.find({ categoryId: category._id });
+    res.json(products);
+  } catch (error) {
+    console.error("Error fetching products by category:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = {
-    getAllProducts,
-    getProductById,
-    createProduct,
-    updateProduct,
-    deleteProduct,
-    getAllCate,
-    getCategoryById,
-    createCategory,
-    getProductsByCategory,
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getAllCate,
+  getCategoryById,
+  createCategory,
+  getProductsByCategory,
 };
